1cf686114a22638221d6d0cabd85cfbdda88cef5
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2025-01-02 13:59:43 +0100

    Update CommonValues.cs

--- Source/Shared/Misc/CommonValues.cs
+++ Source/Shared/Misc/CommonValues.cs
@@ -2,7 +2,7 @@ namespace Shared
 {
     public static class CommonValues
     {
-        public readonly static string executableVersion = "dev";
+        public readonly static string executableVersion = "25.1.2.1";
 
         public static readonly string defaultParserMethodName = "ParsePacket";
 
