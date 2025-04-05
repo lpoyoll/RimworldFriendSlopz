d75d66607b9b40699b98c8e7f45cd63195098e7d
Nova <159603018+Byte-Nova@users.noreply.github.com>
2025-04-05 21:21:51 +0200

    Update Attributes.cs

--- Source/Shared/Misc/Attributes.cs
+++ Source/Shared/Misc/Attributes.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Shared
 {
+    // Used for instantiating managers
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class RTManager : Attribute { }
+
     // Used for loading in custom assemblies
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class RTStartupAttribute : Attribute { }
