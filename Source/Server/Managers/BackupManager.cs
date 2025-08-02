1901db424966afd1b277f713bbfb026ee1bb0583
Nova <159603018+Byte-Nova@users.noreply.github.com>
2025-08-02 13:21:22 +0200

    Fix for server attempting to backup right at boot

--- Source/Server/Managers/BackupManager.cs
+++ Source/Server/Managers/BackupManager.cs
@@ -120,10 +120,10 @@ namespace GameServer.Managers
         {
             while (true)
             {
+                Thread.Sleep(TimeSpan.FromHours(Master.BackupConfig.IntervalHours));
+
                 try { BackupServer(); }
                 catch (Exception e) { Printer.Error($"Backup tick failed, this should never happen. Exception > {e}"); }
-
-                Thread.Sleep(TimeSpan.FromHours(Master.BackupConfig.IntervalHours));
             }
         }
     }
