ddb885ef7486e8fc29846227dd7d903b35bf1b78
Nova <159603018+Byte-Nova@users.noreply.github.com>
2025-08-08 14:46:42 +0200

    Split textures for 1.5 and 1.6

--- Source/Client/Patches/GameStatusPatches.cs
+++ Source/Client/Patches/GameStatusPatches.cs
@@ -53,65 +53,6 @@ namespace GameClient.Patches
             }
         }
 
-        [HarmonyPatch(typeof(SettleInEmptyTileUtility), nameof(SettleInEmptyTileUtility.Settle))]
-        public static class SettlePatch
-        {
-            [HarmonyPostfix]
-            public static void ModifyPost(Caravan caravan)
-            {
-                if (SessionValues.CurrentNetworkState == ClientNetworkState.Connected)
-                {
-                    SettlementManager.SendNewPlayerSettlement(caravan.Tile);
-
-                    SaveManager.ForceSave();
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(SettleInExistingMapUtility), nameof(SettleInExistingMapUtility.Settle))]
-        public static class SettleInMapPatch
-        {
-            [HarmonyPostfix]
-            public static void ModifyPost(Map map)
-            {
-                if (SessionValues.CurrentNetworkState == ClientNetworkState.Connected)
-                {
-                    SettlementManager.SendNewPlayerSettlement(map.Tile);
-
-                    SaveManager.ForceSave();
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(SettlementAbandonUtility), "Abandon")]
-        public static class AbandonPatch
-        {
-            [HarmonyPostfix]
-            public static void ModifyPost(Settlement settlement)
-            {
-                if (SessionValues.CurrentNetworkState != ClientNetworkState.Connected) return;
-                else SettlementManager.AbandonSettlement(settlement.Tile);
-            }
-        }
-
-        [HarmonyPatch(typeof(Settlement), nameof(Settlement.PostRemove))]
-        public static class DestroyNPCSettlementPatch
-        {
-            [HarmonyPostfix]
-            public static void ModifyPost(Settlement __instance)
-            {
-                if (SessionValues.CurrentNetworkState == ClientNetworkState.Connected)
-                {
-                    if (!ClientValues.IsReadyToPlay) return;
-                    if (!SessionValues.ActionValues.EnableNPCDestruction) return;
-
-                    if (__instance.Faction == Faction.OfPlayer) return;
-                    else if (ClientValues.PlayerFactions.Contains(__instance.Faction)) return;
-                    else if (NPCManagerH.lastRemovedSettlement != __instance) NPCManager.RequestSettlementRemoval(__instance);
-                }
-            }
-        }
-
         [HarmonyPatch(typeof(Dialog_Options), nameof(Dialog_Options.DoWindowContents))]
         public static class PatchDevMode
         {
