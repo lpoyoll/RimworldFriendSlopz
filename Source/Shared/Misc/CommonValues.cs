c58f9038bd6f402bcd2419c705597f0e7400556d
Nova <159603018+Byte-Nova@users.noreply.github.com>
2025-12-13 13:30:22 +0100

    Update CommonValues.cs

--- Source/Shared/Misc/CommonValues.cs
+++ Source/Shared/Misc/CommonValues.cs
@@ -2,7 +2,7 @@ namespace Shared
 {
     public static class CommonValues
     {
-        public static string ExecutableVersion { get; set; } = "dev";
+        public static string ExecutableVersion { get; set; } = "25.12.13.1";
 
         public static string DefaultSaveFormat { get; set; } = ".json";
 
