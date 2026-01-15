4ebe458a4b3dc3fe9ae77adf081b300027e3ab4a
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-01-15 04:39:38 +0100

    Planet generation fixes

--- Source/Client/Patches/Pages/SelectStartingSitePatches.cs
+++ Source/Client/Patches/Pages/SelectStartingSitePatches.cs
@@ -11,76 +11,73 @@ using static Shared.CommonEnumerators;
 
 namespace GameClient.Patches.Pages
 {
-    public class SelectStartingSitePatches
+    [HarmonyPatch(typeof(Page_SelectStartingSite), "PreOpen")]
+    public static class PatchSettlements
     {
-        [HarmonyPatch(typeof(Page_SelectStartingSite), "DoCustomBottomButtons")]
-        public static class PathSelectStartingSitePage
+        [HarmonyPostfix]
+        public static void DoPost()
         {
-            [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator)
+            if (!SessionHandler.IsGeneratingFreshWorld)
             {
-                const string disconnectText = "Disconnect";
-                List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
-                MethodInfo helper = AccessTools.Method(typeof(PathSelectStartingSitePage), nameof(Helper));
-                int index = 0;
-                for (; index < codes.Count; index++)
+                WorldManager.SetPlanetFeatures();
+                WorldManager.SetPlanetFactions();
+            }
+
+            PlanetManager.BuildPlanet();
+        }
+    }
+
+    [HarmonyPatch(typeof(Page_SelectStartingSite), "DoCustomBottomButtons")]
+    public static class PathSelectStartingSitePage
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator)
+        {
+            const string disconnectText = "Disconnect";
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            MethodInfo helper = AccessTools.Method(typeof(PathSelectStartingSitePage), nameof(Helper));
+            int index = 0;
+            for (; index < codes.Count; index++)
+            {
+                if (codes[index].operand is string str && str == "Back")
                 {
-                    if (codes[index].operand is string str && str == "Back")
-                    {
-                        CodeInstruction[] newInstructions = new CodeInstruction[]{
+                    CodeInstruction[] newInstructions = new CodeInstruction[]{
                             new(OpCodes.Ldstr, disconnectText) // Swap the text
                         };
-                        
-                        TranspilerHelper.CheckIfConnected(ilGenerator, codes, newInstructions, ref index, 3);
-                        break;
-                    }
+
+                    TranspilerHelper.CheckIfConnected(ilGenerator, codes, newInstructions, ref index, 3);
+                    break;
                 }
+            }
 
-                bool flag = false;
-                for (; index < codes.Count; index++)
+            bool flag = false;
+            for (; index < codes.Count; index++)
+            {
+                if (codes[index].opcode == OpCodes.Ldarg_0)
                 {
-                    if (codes[index].opcode == OpCodes.Ldarg_0)
+                    if (!flag)
                     {
-                        if (!flag)
-                        {
-                            flag = true;
-                            continue;
-                        }
+                        flag = true;
+                        continue;
+                    }
 
-                        CodeInstruction[] newInstructions = new CodeInstruction[]
-                        {
+                    CodeInstruction[] newInstructions = new CodeInstruction[]
+                    {
                             new(OpCodes.Call, helper), // Call helper so Nova can read it
                             new(OpCodes.Ret) // Return
-                        };
-                        TranspilerHelper.CheckIfConnected(ilGenerator, codes, newInstructions, ref index, 0);
-                        break;
-                    }
+                    };
+                    TranspilerHelper.CheckIfConnected(ilGenerator, codes, newInstructions, ref index, 0);
+                    break;
                 }
-
-                return codes;
             }
 
-            private static void Helper()
-            {
-                SceneManager.LoadScene(0);
-                ClientNetwork.Instance.ClientListener.DisconnectNow();
-            }
+            return codes;
         }
 
-        [HarmonyPatch(typeof(Page_SelectStartingSite), "PreOpen")]
-        public static class PatchSettlements
+        private static void Helper()
         {
-            [HarmonyPostfix]
-            public static void DoPost()
-            {
-                if (!SessionHandler.IsGeneratingFreshWorld)
-                {
-                    WorldManager.SetPlanetFeatures();
-                    WorldManager.SetPlanetFactions();
-                }
-
-                PlanetManager.BuildPlanet();
-            }
+            SceneManager.LoadScene(0);
+            ClientNetwork.Instance.ClientListener.DisconnectNow();
         }
     }
 }
