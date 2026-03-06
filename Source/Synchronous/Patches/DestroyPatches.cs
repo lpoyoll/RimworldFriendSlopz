9ef818595a687ac19d4a29c6c16eb1e4a19d98a6
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-03-06 18:42:00 +0100

    Create DestroyPatches.cs

---
+++ Source/Synchronous/Patches/DestroyPatches.cs
@@ -0,0 +1,31 @@
+﻿//using GameClient.Misc;
+//using HarmonyLib;
+//using Synchronous.Core;
+//using Synchronous.Managers;
+//using Synchronous.Misc;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Verse;
+
+//namespace Synchronous.Patches
+//{
+//    [HarmonyPatch(typeof(Thing), nameof(Thing.Destroy))]
+//    public static class P_Thing_Destroy
+//    {
+//        [HarmonyPrefix]
+//        public static bool Destroy(Thing __instance)
+//        {
+//            if (PatchHandler.BypassFlag) return true;
+//            else if (!Main_.CheckIfShouldPatch(__instance.Map)) return true;
+//            else if (!SessionHandler.IsSynchronousHost) return false;
+//            else
+//            {
+//                SDestroyManager.Ask(__instance);
+//                return false;
+//            }
+//        }
+//    }
+//}
