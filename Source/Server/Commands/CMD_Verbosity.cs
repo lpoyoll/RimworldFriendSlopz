e34048b28da677a5ae2c7dc596efdcacdd99a8bf
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-03-22 15:09:25 +0100

    Create CMD_Verbosity.cs

---
+++ Source/Server/Commands/CMD_Verbosity.cs
@@ -0,0 +1,27 @@
+﻿using GameServer.Core;
+using Shared;
+using Shared.Files.Configs;
+using Shared.Misc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTServerBrowser.Commands
+{
+    public class CMD_Verbosity : CMD_Base
+    {
+        public CMD_Verbosity()
+        {
+            Prefix = "verbosity";
+            Description = "Changes the verbosity to the desired value";
+        }
+
+        public override void Action()
+        {
+            Master.ServerConfig.Verbosity = int.Parse(CommandParameters[0]);
+            ServerConfigFile.Save(ServerConfigFile.SavePath, Master.ServerConfig);
+
+            Printer.Title($"Verbosity changed to {Master.ServerConfig.Verbosity}");
+        }
+    }
+}
