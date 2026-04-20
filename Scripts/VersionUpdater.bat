ea7192867c9fb726ab56db22eb41e388035a1417
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-04-20 20:30:55 +0200

    Update VersionUpdater.bat

--- Scripts/VersionUpdater.bat
+++ Scripts/VersionUpdater.bat
@@ -8,7 +8,7 @@ title RimWorld Together - Version Updater
 
 ::Set mod folder path
 set /p ModFolder=<ModPath.txt
-echo "Mods folder located at %ModFolder%"
+echo Mods folder located at "%ModFolder%"
 del ModPath.txt
 
 ::Wait for RimWorld safe close
