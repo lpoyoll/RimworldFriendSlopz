f36464a7a3c63c926612b9cc4ae09815616cd5da
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-05-16 04:28:39 +0200

    Added Discord Rich Presence

---
+++ Source/Client/Misc/DiscordHandler.cs
@@ -0,0 +1,50 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using Shared;
+using Shared.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClient.Misc
+{
+    public static class DiscordHandler
+    {
+        private static DiscordRpcClient RPClient { get; set; } = null;
+
+        private static readonly string PresenceID = "1505021874868981854";
+
+        [OnSessionStart]
+        private static void StartPresence()
+        {
+            RPClient = new DiscordRpcClient(PresenceID);
+            RPClient.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            RPClient.Initialize();
+
+            RPClient.SetPresence(new RichPresence()
+            {
+                Details = "Playing Multiplayer",
+                Timestamps = Timestamps.Now,
+                Buttons = new Button[]
+                {
+                    new Button()
+                    {
+                        Label = "Download",
+                        Url = "https://steamcommunity.com/sharedfiles/filedetails/?id=3005289691"
+                    }
+                }
+            });
+
+            Printer.Message("Discord Rich Presence has been started", Printer.LogImportanceMode.Verbose);
+        }
+
+        [OnSessionEnd]
+        private static void StopPresence()
+        {
+            RPClient.Dispose();
+            Printer.Message("Discord Rich Presence has been stopped", Printer.LogImportanceMode.Verbose);
+        }
+    }
+}
