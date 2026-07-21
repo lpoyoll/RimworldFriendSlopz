542c6ea55b9ac4d5069d0a91b805090c17a7d56b
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-07-21 23:12:47 +0200

    Minor class renaming

---
+++ Source/Server/Commands/CMD_ToggleBrowser.cs
@@ -0,0 +1,25 @@
+﻿using RTServer.Core;
+using RTServer.Files;
+using RTShared.Commands;
+using RTShared.Misc;
+
+namespace RTServer.Commands
+{
+    public class CMD_ToggleBrowser : CMD_Base
+    {
+        public CMD_ToggleBrowser()
+        {
+            Prefix = "togglebrowser";
+            Description = "Toggles the server browser discovery feature";
+        }
+
+        public override void Action()
+        {
+            Master.ServerConfig.EnableServerBrowser = !Master.ServerConfig.EnableServerBrowser;
+            FL_ServerConfig.Save(FL_ServerConfig.SavePath, Master.ServerConfig);
+
+            string display = Master.ServerConfig.EnableServerBrowser ? "ON" : "OFF";
+            Printer.Warning($"Server browser discovery is now {display}");
+        }
+    }
+}
