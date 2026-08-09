7fd1aa436158dfb08eab492469d6707df0cf3f73
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-08-09 05:33:51 +0200

    Added player card feature

--- Source/Server/PacketManagers/PM_SettlementCustomization.cs
+++ Source/Server/PacketManagers/PM_SettlementCustomization.cs
@@ -13,7 +13,22 @@ namespace RTServer.PacketManagers
         public override void Receive(ServerClient client, byte[] bytes, PacketHeader header)
         {
             PKT_SettlementCustomization packet = Serializer.ConvertBytesToObject<PKT_SettlementCustomization>(bytes);
-            ChangeCustomizations(client, packet);
+            
+            switch (packet.CurrentStep)
+            {
+                case PKT_SettlementCustomization.StepMode.Set:
+                    ChangeCustomizations(client, packet);
+                    break;
+                
+                case PKT_SettlementCustomization.StepMode.Refresh:
+                    SendCustomizations(client, packet);
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            
         }
 
         private static void ChangeCustomizations(ServerClient client, PKT_SettlementCustomization packet)
@@ -25,6 +40,15 @@ namespace RTServer.PacketManagers
             
             client.Listener.EnqueuePacket(PacketHeader.SettlementCustomization, packet);
             InformationDisplayer.DisplayChangedCustomizations(playerFile.Username);
+        }
+
+        private static void SendCustomizations(ServerClient client, PKT_SettlementCustomization packet)
+        {
+            FL_Player playerFile = client.GetData<FL_Player>();
+
+            packet.IconID = playerFile.Customizations.SettlementIconID;
+            packet.IconColor = playerFile.Customizations.SettlementIconColor;
+            client.Listener.EnqueuePacket(PacketHeader.SettlementCustomization, packet);
         }
     }
 }
