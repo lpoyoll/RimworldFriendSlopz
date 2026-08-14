0749320bd3b9829627e30c52cd716f5bfc190a90
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-08-14 16:20:13 +0200

    Minor changes to core and saving

--- Source/Server/Files/FL_ServerConfig.cs
+++ Source/Server/Files/FL_ServerConfig.cs
@@ -26,7 +26,7 @@ namespace RTServer.Files
 
         public bool UseUPnP { get; set; } = false;
 
-        public bool UseClientSave { get; set; } = false;
+        public bool UseClientSave { get; set; } = true;
 
         public bool EnableServerBrowser { get; set; } = true;
 
